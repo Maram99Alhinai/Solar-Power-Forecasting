9940df05219a64209b987de71216fc37c4a3c479
tutalae <kksaikheaw@gmail.com>
2023-06-27 11:46:56 +0000

    fixed preprocessing file 27/6/2023 18.46

--- evaluation.py
+++ evaluation.py
@@ -1,44 +1,46 @@
 
-import xgboost as xgb
-from sklearn.metrics import mean_squared_error, r2_score
+import json
+import os
+import pathlib
+import pickle as pkl
+import tarfile
+import joblib
+import numpy as np
+import pandas as pd
+import datetime as dt
+from sklearn.metrics import mean_squared_error
 
-if __name__ == "__main__":   
-    
+if __name__ == "__main__":
     # All paths are local for the processing container
-    model_path = f"{prefix}/processing/model/model.tar.gz"
-    test_x_path = f"{prefix}processing/test/test_x.csv"
-    test_y_path = f"{prefix}/processing/test/test_y.csv"
-    output_dir = f"{prefix}/processing/evaluation"
-    output_prediction_path = f"{prefix}/processing/output/"
-        
+    model_path = "/opt/ml/processing/model/model.tar.gz"
+    test_x_path = "/opt/ml/processing/test/test_x.csv"
+    test_y_path = "/opt/ml/processing/test/test_y.csv"
+    output_dir = "/opt/ml/processing/evaluation"
+    output_prediction_path = "/opt/ml/processing/output/"
+    
     # Read model tar file
     with tarfile.open(model_path, "r:gz") as t:
         t.extractall(path=".")
     
     # Load model
-    model = linear.Booster()
-    model.load_model("linear-model")
+    model = joblib.load("linear-model")
     
     # Read test data
     X_test = pd.read_csv(test_x_path, header=None).values
-    y_test = pd.read_csv(test_y_path, header=None).to_numpy()
+    y_test = pd.read_csv(test_y_path, header=None).to_numpy().squeeze()
 
     # Run predictions
-    predictions = model.predict(X_test)
+    prediction = model.predict(X_test)
 
-    # Calculate RMSE
-    rmse = np.sqrt(mean_squared_error(y_test, predictions))
-    
-    # Calculate R2 score
-    r2 = r2_score(y_test, predictions)
-    
+    # Get predicted labels (convert probabilities to binary predictions)
+    predicted_labels = (prediction > 0.5).astype(int)
+
+    # Evaluate predictions
+    rmse_score = np.sqrt(mean_squared_error(y_test, predicted_labels))
     report_dict = {
         "regression_metrics": {
-            "rmse": {
-                "value": rmse,
-            },
-            "r2_score": {
-                "value": r2,
+            "rmse_score": {
+                "value": rmse_score,
             },
         },
     }
@@ -47,8 +49,10 @@ if __name__ == "__main__":
     pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
     with open(f"{output_dir}/evaluation.json", "w") as f:
         f.write(json.dumps(report_dict))
-    
+
     # Save prediction baseline file - we need it later for the model quality monitoring
-    pd.DataFrame({"prediction": predictions,
-                  "label": y_test.squeeze()}
-                ).to_csv(os.path.join(output_prediction_path, 'prediction_baseline/prediction_baseline.csv'), index=False, header=True)
+    pd.DataFrame({
+        "prediction": predicted_labels,
+        "probability": prediction,
+        "label": y_test,
+    }).to_csv(os.path.join(output_prediction_path, "prediction_baseline/prediction_baseline.csv"), index=False, header=True)
--- preprocessing.py
+++ preprocessing.py
@@ -4,20 +4,14 @@ import numpy as np
 import argparse
 import os
 
-bucket_name = 'sagemaker-us-east-1-531485126105' # my s3
-
-# if have no input this will be default
-input_url = f"s3://{bucket_name}/Group-project/Solar1/processing/input/"
-output_url = f"s3://{bucket_name}/Group-project/Solar1/processing/output/"
-
 def _parse_args():
     
     parser = argparse.ArgumentParser()
     # Data, model, and output directories
-    # model_dir is always passed in from SageMaker. By default, this is an S3 path under the default bucket.
-    parser.add_argument('--filepath', type=str, default='/Group-project/Solar1/processing/input/')
-    parser.add_argument('--filename', type=str, default='combined_plant')
-    parser.add_argument('--outputpath', type=str, default='Group-project/Solar1/processing/output/')
+    # model_dir is always passed in from SageMaker. By default this is a S3 path under the default bucket.
+    parser.add_argument('--filepath', type=str, default='/opt/ml/processing/input/')
+    parser.add_argument('--filename', type=str, default='combined_plant.csv')
+    parser.add_argument('--outputpath', type=str, default='/opt/ml/processing/output/')
     
     return parser.parse_known_args()
 
@@ -27,15 +21,24 @@ if __name__=="__main__":
     args, _ = _parse_args()
     
     target_col = "DC_POWER"
-    input_rul = input_url
     
     # # Load data
-    combined_plant = pd.read_csv(os.path.join(args.filepath, args.filename), sep=";")
+    df_model_data = pd.read_csv(os.path.join(args.filepath, args.filename))
     
-    # Shuffle and split the dataset
+    combined_plant.drop(['SOURCE_KEY', 'DATE_TIME'], axis=1)
+    
+    print(df_model_data.columns)
+    print(df_model_data.head(5))
+
+    # Save the baseline dataset for model monitoring
+    df_model_data.drop([target_col], axis=1).to_csv(os.path.join(args.outputpath, 'baseline/baseline.csv'), index=False, header=False)
+    
+    print("## Processing complete. Exiting.")
+
+    # Shuffle and splitting dataset
     train_data, validation_data, test_data = np.split(
-        combined_plant.sample(frac=1, random_state=1729),
-        [int(0.7 * len(combined_plant)), int(0.9 * len(combined_plant))],
+        df_model_data.sample(frac=1, random_state=1729),
+        [int(0.7 * len(df_model_data)), int(0.9 * len(df_model_data))],
     )
 
     print(f"Data split > train:{train_data.shape} | validation:{validation_data.shape} | test:{test_data.shape}")
@@ -45,46 +48,8 @@ if __name__=="__main__":
     validation_data.to_csv(os.path.join(args.outputpath, 'validation/validation.csv'), index=False, header=False)
     test_data[target_col].to_csv(os.path.join(args.outputpath, 'test/test_y.csv'), index=False, header=False)
     test_data.drop([target_col], axis=1).to_csv(os.path.join(args.outputpath, 'test/test_x.csv'), index=False, header=False)
-
-
-    # Save the baseline dataset for model monitoring
-    combined_plant.drop([target_col], axis=1).to_csv(os.path.join(args.outputpath, 'baseline/baseline.csv'), index=False, header=False)
+    
+    # Save the baseline dataset for model monitoring
+    df_model_data.drop([target_col], axis=1).to_csv(os.path.join(args.outputpath, 'baseline/baseline.csv'), index=False, header=False)
     
     print("## Processing complete. Exiting.")
-
-    
-    # # Shuffle the dataset
-# shuffled_plant = combined_plant.sample(frac=1).reset_index(drop=True)
-
-# # Split the shuffled dataset
-# train_data, validation_data, test_data = np.split(shuffled_plant, [int(0.7 * len(shuffled_plant)), int(0.9 * len(shuffled_plant))])
-
-# print(f"Data split > train: {train_data.shape} | validation: {validation_data.shape} | test: {test_data.shape}")
-
-# print("## Processing complete. Exiting.")
-
-# # Create a folder to save the files
-# data_folder = "cleaned_data"  # Specify the data folder path
-# os.makedirs(data_folder, exist_ok=True)
-
-# # Define the file paths
-# train_x_file = os.path.join(data_folder, "train_x.csv")
-# train_y_file = os.path.join(data_folder, "train_y.csv")
-# validation_x_file = os.path.join(data_folder, "validation_x.csv")
-# validation_y_file = os.path.join(data_folder, "validation_y.csv")
-# test_x_file = os.path.join(data_folder, "test_x.csv")
-# test_y_file = os.path.join(data_folder, "test_y.csv")
-# baseline_file = os.path.join(data_folder, "baseline.csv")
-# file_path = os.path.join(data_folder, "combined_plant.csv")
-
-# # Save datasets locally
-# train_data.drop([target_col], axis=1).to_csv(train_x_file, index=False)
-# train_data[target_col].to_csv(train_y_file, index=False)
-# validation_data.drop([target_col], axis=1).to_csv(validation_x_file, index=False)
-# validation_data[target_col].to_csv(validation_y_file, index=False)
-# test_data.drop([target_col], axis=1).to_csv(test_x_file, index=False)
-# test_data[target_col].to_csv(test_y_file, index=False)
-# combined_plant.drop([target_col], axis=1).to_csv(baseline_file, index=False)
-# combined_plant.to_csv(file_path, index=False)
-
-# print("All files saved successfully to the local folder:", data_folder)
